595e0aa19b9300d80443bc402845baeadf73ef63
sudiksha-chindula <sudiksha.chindula7@gmail.com>
2026-04-10 11:40:48 +0530

    Fix parser logic for instr type and line number

--- src/build-engine/parser.c
+++ src/build-engine/parser.c
@@ -5,17 +5,20 @@
 
 char* keyword;
 char buffer[100];
-char* keyword;
 char* line;
 char* remline;
+char* remtok;
 struct parsed* all[50];
+
 
 int main()
 {
     int count=5;
     char path[300]="/Users/sudiksha/sudiksha/acad/sem-6/cloud-computing/project/docker-from-scratch/src/Docksmithfile";
     struct parsed* result = parse(path, &count);
-    printf("Result returned successfully");
+    printf("Result returned successfully.\n");
+    printf("Instr: %s", result->instr);
+    printf("Type: %d", result->instrtype);
     return 0;
 }
 
@@ -24,62 +27,62 @@ struct parsed* parse(char* path, int* count)
     FILE* ds_file=fopen(path, "r");
     int i=0;
 
-    //file pointer null
+    //file pointer null case
     if (ds_file==NULL)
     {
         perror("fopen");
         return NULL;
     }
-    struct parsed* current = malloc(sizeof(struct parsed));
-    do
+
+    struct parsed* main = malloc(sizeof(struct parsed)*50);
+    line = fgets(buffer, sizeof(buffer), ds_file);
+
+    while (line!=NULL)
     {
-        line = fgets(buffer, sizeof(buffer), ds_file);
-        printf("%s", line);
+        main[i].instr=line;
+        printf("Line number: %d\n", i);
+
+        //keyword determination
         keyword = strtok(line, " ");
-        remline=keyword;
         printf("keyword: %s\n", keyword);
-       
-        while(remline!=NULL)
+        
+        //remaining line determination
+        remtok=keyword;
+        while((remtok=strtok(NULL, " "))!=NULL)    //loop check after strtok
         {
-            remline = strtok(NULL, " ");
-            printf("remline: %s\n", remline);
+            printf("remtok: %s\n", remtok);
         }
         
-        current->instr=line;
+        //instruction type determination
         if (strcmp(keyword, "FROM")==0)
         {
-            current->instrtype=FROM;
+            main[i].instrtype=FROM;
         }
-        else if (strcmp(keyword,"COPY"))
+        else if (strcmp(keyword,"COPY")==0)
         {
-            current->instrtype=COPY;
+            main[i].instrtype=COPY;
         }
         else if (strcmp(keyword, "RUN")==0)
         {
-            current->instrtype=RUN;
+            main[i].instrtype=RUN;
         }
         else if (strcmp(keyword,"WORKDIR")==0)
         {
-            current->instrtype=WORKDIR;
+            main[i].instrtype=WORKDIR;
         }
         else if (strcmp(keyword, "ENV")==0)
         {
-            current->instrtype=ENV;
+            main[i].instrtype=ENV;
         }
-        else
+        else 
         {
             printf("Invalid instruction in Docksmithfile. Please check.\n");
         }
-        all[i]=current;
         i++;
-        //sscanf(buffer, "%s %[^\n]", keyword, remline);
-        //printf("%s keyword\n", keyword);
-        //printf("%s remaining line\n", line);
+    
         
+        line=fgets(buffer, sizeof(buffer), ds_file);
         
     }
-    while (line!=NULL);
-
-    //temp return
-    return current;
+    return main;
 }
