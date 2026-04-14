cdc89dc775abb2ff18e192628e5b33112015241a
sudiksha-chindula <sudiksha.chindula7@gmail.com>
2026-04-14 20:43:08 +0530

    .tar creation

--- src/build-engine/layer.c
+++ src/build-engine/layer.c
@@ -1,95 +1,114 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <sys/stat.h>   //temporarily unused
+#include <sys/stat.h>
 #include <archive.h>
 #include <archive_entry.h>
 #include <string.h>
 #include <stdlib.h>
 
-//executor engine returns the directory path
-
-char* filename;
+char *filename; //appended to dir path
 char afilename[256];
 char dir_name[2048];
-//char* filenames[100];
-struct archive* a;
-struct archive_entry* entry;
-const char* outname = "/Users/sudiksha/sudiksha/acad/sem-6/cloud-computing/project/docker-from-scratch/src/dummy-dir.tar";
-struct stat st;
-char buff[8192];
-int len;
-FILE* fd;
 
+const char *outname = "dummy-dir.tar.gz";
 
-void write_archive(const char *outname, const char **afilename)
+//refer libarchive documentation (rem to add to README)
+void write_archive(const char *outname, const char *path)
 {
-  struct archive *a;
-  struct archive_entry *entry;
-  struct stat st;
-  char buff[8192];
-  int len;
-  int fd;
+    struct archive *a;
+    struct archive_entry *entry;
+    struct stat st;
+    char buff[8192];
+    size_t len;
+    FILE *fd;
 
-  a = archive_write_new();
-  archive_write_add_filter_gzip(a);
-  archive_write_set_format_pax_restricted(a); // Note 1
-  archive_write_open_filename(a, outname);
-  while (*filename) {
-    stat(*filename, &st);
-    entry = archive_entry_new(); // Note 2
-    archive_entry_set_pathname(entry, *filename);
-    archive_entry_set_size(entry, st.st_size); // Note 3
+    //file stat data check
+    if (stat(path, &st) != 0)
+    {
+        perror("stat");
+        return;
+    }
+
+    //creating archive objects
+    a = archive_write_new();
+    archive_write_add_filter_gzip(a);
+    archive_write_set_format_pax_restricted(a);
+
+    if (archive_write_open_filename(a, outname) != ARCHIVE_OK)
+    {
+        printf("Could not open archive output file\n");
+        archive_write_free(a);
+        return;
+    }
+
+    //creating archive entry
+    entry = archive_entry_new();
+
+    archive_entry_set_pathname(entry, path);
+    archive_entry_set_size(entry, st.st_size);
     archive_entry_set_filetype(entry, AE_IFREG);
     archive_entry_set_perm(entry, 0644);
+
     archive_write_header(a, entry);
-    FILE* fd = fopen(*filename, "r");
-    len = read(fd, buff, sizeof(buff));
-    while ( len > 0 ) {
-        archive_write_data(a, buff, len);
-        len = read(fd, buff, sizeof(buff));
+
+    //file archive write
+    fd = fopen(path, "rb");
+    if (!fd)
+    {
+        perror("fopen");
+        archive_entry_free(entry);
+        archive_write_close(a);
+        archive_write_free(a);
+        return;
     }
-    close(fd);
+
+    //archive data write
+    while ((len = fread(buff, 1, sizeof(buff), fd)) > 0)
+    {
+        archive_write_data(a, buff, len);
+    }
+
+    fclose(fd);
     archive_entry_free(entry);
-    filename++;
-  }
-  archive_write_close(a); // Note 4
-  archive_write_free(a); // Note 5
+
+    printf("Successfully wrote entry: %s\n", path);
+
+    archive_write_close(a);
+    archive_write_free(a);
 }
 
 int main()
 {
-    strcpy(dir_name,"/Users/sudiksha/sudiksha/acad/sem-6/cloud-computing/project/docker-from-scratch/src/dummy-dir");
-    struct dirent* dp;
-    DIR *dfd;
+    strcpy(dir_name,
+    "/Users/sudiksha/sudiksha/acad/sem-6/cloud-computing/project/docker-from-scratch/src/dummy-dir");
 
-    dfd = opendir(dir_name);
+    struct dirent *dp;
+    DIR *dfd = opendir(dir_name);
 
-    //null condition check for directory pointer
-    if (dfd==NULL)
+    if (dfd == NULL)
     {
         printf("Invalid Directory, couldn't be resolved\n");
-        return 0;
+        return 1;
     }
-    else
+
+    strcat(dir_name, "/");
+
+    printf("Reading directory: %s\n", dir_name);
+
+    while ((dp = readdir(dfd)) != NULL)
     {
-        strcat(dir_name, "/");
-        printf("%s\n", dir_name);
-        while ((dp = readdir(dfd))!=NULL)
+        filename = dp->d_name;
+
+        if (strcmp(filename, ".") != 0 &&
+            strcmp(filename, "..") != 0)
         {
-            filename = dp->d_name;
-            if (strcmp(filename, ".")!=0 && strcmp(filename, "..")!=0)
-            {
+            strcpy(afilename, dir_name);
+            strcat(afilename, filename);
 
-                //giving complete file path in afilename
-                strcpy(afilename, dir_name);
-                strcat(afilename, filename);
-
-            }                
-            }
+            write_archive(outname, afilename);
         }
-        return 0;
-        
     }
 
-   
-
+    closedir(dfd);
+    return 0;
+}
